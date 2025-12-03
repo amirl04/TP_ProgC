58aa462c0d0e99d9ef1a57badedf6b03e567ac36
amirl04 <ladraa.amir92@gmail.com>
2025-12-03 21:06:54 +0100

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5;      // hauteur de la pyramide
+    int i, j;
+
+    // Génération de la pyramide
+    for (i = 1; i <= n; i++) {
+
+        // 1) Afficher les espaces pour centrer
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        // 2) Nombres croissants de 1 à i
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // 3) Nombres décroissants de i-1 à 1
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        // Passage à la ligne suivante
+        printf("\n");
+    }
+
+    printf("Generation de la pyramide terminee.\n");
+
+    return 0;
+}
 
