aeb84e4d551c324acfbcfb57e2239a8b28ccdfe7
amirl04 <ladraa.amir92@gmail.com>
2025-12-03 20:31:22 +0100

    Update conditions.c

---
+++ TP1/src/conditions.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int somme = 0;
+
+    for (int i = 1; i <= 1000; i++) {
+
+        // Si divisible par 11 → on ignore et on continue immédiatement
+        if (i % 11 == 0) {
+            continue;
+        }
+
+        // Si divisible par 5 ou 7 → on ajoute
+        if (i % 5 == 0 || i % 7 == 0) {
+            somme += i;
+        }
+
+        // Si somme dépasse 5000 → on arrête tout
+        if (somme > 5000) {
+            break;
+        }
+    }
+
+    printf("Somme finale : %d\n", somme);
+
+    return 0;
+}
